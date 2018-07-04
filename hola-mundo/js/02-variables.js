47a046fca7bd17a719c294b9ee5ba8ed5611548e
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2018-07-04 13:47:43 -0500

    Variables y comentarios
     Declaracion, modificacion y impresion de variables, y comentarios.

---
+++ hola-mundo/js/02-variables.js
@@ -0,0 +1,16 @@
+// Variables
+/*
+    Comentario varias lineas
+*/
+
+var pais = "México";
+var continente = "America";
+var antiguedad = 2019;
+var pais_y_continente = pais + ' ' + continente;
+
+pais = "España";
+continente = "Europa";
+
+console.log(pais, continente, antiguedad);
+console.log(pais_y_continente);
+alert(pais_y_continente);
