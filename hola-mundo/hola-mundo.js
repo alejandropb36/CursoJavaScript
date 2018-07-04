d750a851cba64be58ea5d6ead26ebd57b7041ea3
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2018-07-04 12:22:37 -0500

    La consola del navegador
     Hice impresiones en consola asi como una operación muy basica.

--- hola-mundo/hola-mundo.js
+++ hola-mundo/hola-mundo.js
@@ -1,2 +1,6 @@
 alert("Hola Mundo con JS desde fichero externo");
-document.write("Hola Mundo desde fichero externo usando document.write")
+document.write("Hola Mundo desde fichero externo usando document.write");
+console.log("Mensaje que se va a mostrar en la consola");
+console.log(95 + 5);
+var nombre = "Alejandro Ponce";
+console.log("El desarrollador de este codigo es: " + nombre);
