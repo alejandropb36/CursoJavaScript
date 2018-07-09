9828c4e75052f7514efbc1e70e2ac44acaf3a4c1
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2018-07-09 13:59:06 -0500

    Tipos de datos y operadores

---
+++ hola-mundo/js/05-tipos-de-datos.js
@@ -0,0 +1,32 @@
+'use strict'
+
+// Operadores 
+
+var numero1 = 34;
+var numero2 = 55;
+var operacion = numero1 * numero2;
+
+alert("El resultado es: " + operacion);
+
+// Tipos de datos
+
+var numer_entero = 21;
+var cadena_texto = "Hola 'que' tal";
+var verdadero_o_falso = true;
+var numero_flotante = 0.9857743;
+
+console.log(numer_entero);
+console.log(cadena_texto);
+console.log(verdadero_o_falso);
+console.log(numero_flotante);
+
+var numero_cadena = "550.784";
+
+console.log(numero_cadena + 24);
+console.log(Number(numero_cadena) + 24);
+console.log(parseInt(numero_cadena) + 24);
+console.log(parseFloat(numero_cadena) + 24);
+
+console.log(String(numer_entero) + 23);
+
+// Mensaje de prueba alert('Hola');
