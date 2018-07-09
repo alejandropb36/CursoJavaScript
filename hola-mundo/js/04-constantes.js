a8b74cfad24ce4fcf38e322c5618300c2aea7d4d
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2018-07-08 21:56:08 -0500

    Constantes.
     Declaracion y uso de una constante.

---
+++ hola-mundo/js/04-constantes.js
@@ -0,0 +1,14 @@
+'use strict'
+
+//Constantes
+// Es un contenedor de datos el cual no va a cambiar 
+
+var web = "https://www.alejandroponce.com";
+const ip = "192.168.101.1";
+
+console.log(web, ip);
+
+web = "https://www.alejandro.com";
+ip = "192.168.3.1";
+
+console.log(web, ip); // Esto va a arrojar un error
