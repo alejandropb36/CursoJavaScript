86bdcba20f799f0cfd389f3d9c1ce3380a6c0caa
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2018-09-04 13:47:39 -0500

    Ventanas en Js

---
+++ hola-mundo/js/10-ventanas.js
@@ -0,0 +1,14 @@
+'use strict'
+
+//Alert
+//alert("Esto es uan alerta");
+
+//confirmacion 
+/*
+* var confirmacion = confirm("¿Estas seguro de continuar?");
+* console.log(confirmacion);
+*/
+
+//Ingreso de datos
+var edad = prompt("¿Que edad tienes?", 18);
+console.log(typeof edad);
