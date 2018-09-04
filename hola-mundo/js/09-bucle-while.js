d7897e07f7a0a24a254dafc52fa28141f712ddf7
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2018-09-04 10:59:27 -0500

    Bucle While y Do - While

--- hola-mundo/js/09-bucle-while.js
+++ hola-mundo/js/09-bucle-while.js
@@ -2,3 +2,15 @@
 
 // Bucle While
 
+var i = 0;
+while(i < 100){
+    console.log(i);
+    i++;
+}
+
+i = 100;
+
+do{
+    console.log("Si entramos por que hay una iteracion de entrada");
+    console.log("El valor de la variable es: " + i);
+}while(i < 100);
