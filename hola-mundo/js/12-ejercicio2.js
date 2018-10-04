88a3832e95e06c03314167c36748bacbf136fc5d
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2018-10-04 13:08:03 -0500

    Salto de ejercicios

---
+++ hola-mundo/js/12-ejercicio2.js
@@ -0,0 +1,9 @@
+'use strict'
+
+/**
+ * Utilizar un bucle, mostrar la suma y la media de los numeros introducidos
+ * hasta introducir un numero negativo y ahí mostrar el resultado
+ */
+
+ var suma = 0;
+ var contador = 0;
