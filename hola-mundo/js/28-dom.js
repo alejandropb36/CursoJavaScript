b9f54577df0f9547b5fa75ca401683191d5cfa88
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-02-08 12:52:19 -0600

    Fin de DOM

--- hola-mundo/js/28-dom.js
+++ hola-mundo/js/28-dom.js
@@ -19,27 +19,63 @@ caja.style.padding = "20px";
 caja.style.color = "white";
 caja.className = "hola cajas";
 
-console.log(caja);
+//console.log(caja);
 
 //Conseguir elementos por su etiqueta
-
 var todosLosDivs = document.getElementsByTagName('div');
 
+//console.log(todosLosDivs);  //Impresion de los divs
+
 var contenidoEnTexto = todosLosDivs[2];
-contenidoEnTexto.innerHTML = "Otro texto ";
+contenidoEnTexto.innerHTML = "Otro texto para el segundo elemento";
 contenidoEnTexto.style.background = "red";
 
+/**
+ * Recorriendo los elementos que son Divs
+ * y agregandolos a la seccion en forma de
+ * parrafo
+ */
 var seccion = document.querySelector("#miseccion");
 var hr = document.createElement("hr");
 
 seccion.append(hr);
 
-for (var valor in todosLosDivs){
-	if(typeof todosLosDivs[valor].textContent == "string"){
+for (var valor in todosLosDivs)
+{
+	if(typeof todosLosDivs[valor].textContent == "string")
+	{
 		var parrafo = document.createElement("p");
 		var texto = document.createTextNode(todosLosDivs[valor].textContent);
 		parrafo.append(texto);
 		seccion.append(parrafo);
 	}	
 }
+
 seccion.append(hr);
+
+// Conseguir elementos por su clase css
+var divsRojos = document.getElementsByClassName('rojo');
+var divsAmarillos = document.getElementsByClassName('amarillo');
+divsAmarillos[0].style.background = "yellow";
+
+
+for(var div in divsRojos)
+{
+	//console.log(divsRojos[div]);
+	if(divsRojos[div].className ==  'rojo')
+	{
+		divsRojos[div].style.background = "red";
+	}
+	
+}
+
+// console.log(divsRojos);
+
+// Query selector
+var claseRojo = document.querySelector("div.rojo");
+console.log(claseRojo);
+var divs = document.querySelector("div");
+console.log(divs);
+
+claseRojo = document.querySelectorAll(".rojo");
+console.log(claseRojo);
