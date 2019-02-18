11cfffb03e8529e4da32578ae946dd872931d068
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-02-18 14:02:19 -0600

    30- Eventos

--- hola-mundo/js/30-eventos.js
+++ hola-mundo/js/30-eventos.js
@@ -4,3 +4,38 @@
 
 var boton = document.querySelector("#boton");
 
+
+function cambiarColor()
+{
+    var bg = boton.style.background;
+     if(bg == "green")
+     {
+        boton.style.background = "red";
+     }
+     else
+     {
+        boton.style.background = "green";
+     }
+     boton.style.padding = "15px";
+     boton.style.boder = "1px solid #ccc";
+}
+
+// click
+boton.addEventListener('click', function(){
+    cambiarColor();
+});
+
+
+// Mouse oover
+boton.addEventListener('mouseover', function(){
+    
+    boton.style.background = "#ccc";
+    
+});
+
+// Mouseout
+boton.addEventListener('mouseout', function(){
+    
+    boton.style.background = "white";
+    
+});
