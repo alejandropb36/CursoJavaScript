5e7eba5f5cc648124bf5fda9fe0591c7a6e1ae92
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-02-20 13:51:38 -0600

    Timers terminado

--- hola-mundo/js/31-timers.js
+++ hola-mundo/js/31-timers.js
@@ -2,19 +2,37 @@
 
 window.addEventListener('load', () => {
     // Timers
-    var tiempo = setInterval(() => {
-        console.log("Set interval ejecutado");
 
-        var h1 = document.querySelector("h1");
+    function intervalo(){
+        var tiempo = setInterval(() => {
+            console.log("Set interval ejecutado");
+    
+            var h1 = document.querySelector("h1");
+    
+            if(h1.style.fontsize == "50px"){
+                h1.style.fontsize = "25px";
+            }
+            else{
+                h1.style.fontsize = "50px";
+            }
+        }, 1000);
+        return tiempo;
+    }
+    
+    var tiempo = intervalo();
 
-        if(h1.style.fontsize == "50px"){
-            h1.style.fontsize = "25px";
-        }
-        else{
-            h1.style.fontsize = "50px";
-        }
+    var stop = document.querySelector("#stop");
 
-        
+    stop.addEventListener('click', function () {
+        alert("Has parado el intervalo en bucle"); 
+        clearInterval(tiempo);
+    });
 
-    }, 3000);
+    var start = document.querySelector("#start");
+
+    start.addEventListener('click', function () {
+        alert("Has iniciado el intervalo"); 
+        intervalo();
+    });
+
 });
