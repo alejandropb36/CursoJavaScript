0539212ab2a47a099ae46c3e6652225e5a068f4d
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-06-05 12:45:39 -0500

    LocalStorage: Como trabajar con el local storage

--- hola-mundo/js/34-localstorage.js
+++ hola-mundo/js/34-localstorage.js
@@ -20,3 +20,40 @@ localStorage.setItem("titulo", "elemento del localstorage");
 /**
  * Recuperar elemento del localstorage
  */
+var local = localStorage.getItem("titulo");
+console.log(local);
+
+document.querySelector("#peliculas").innerHTML = local;
+
+
+/**
+ * Guardar Objetos en el local storage
+ */
+var usuario = {
+    nombre: "Alejandro Ponce",
+    email: "alexponce36@hotmail.com",
+    web: "alex.com"
+};
+
+// Con el stringify podemos convertir a string en objeto json
+localStorage.setItem("usuario", JSON.stringify(usuario));
+
+/**
+ * Recuperar objeto JSON del localstorage
+ */
+var json = localStorage.getItem("usuario");
+console.log(json);
+
+var userjs = JSON.parse(json);
+
+console.log(userjs);
+
+document.querySelector("#datos").append(" web: " + userjs.web);
+
+/**
+ * Borrar elementos del local storage
+ */
+localStorage.removeItem("usuario")
+
+// Vaciar todo el local storage
+localStorage.clear();
