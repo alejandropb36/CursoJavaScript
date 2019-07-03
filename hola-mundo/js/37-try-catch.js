d8bf53aa699eb198f1b8e3ec26eff8581b1b602d
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-07-03 17:49:42 -0500

    Fechas, Matematicas: Fechas en JavaScript

--- hola-mundo/js/37-try-catch.js
+++ hola-mundo/js/37-try-catch.js
@@ -3,8 +3,8 @@
 try {
     var year = 2019;
 
-    var vector = Array(99999999999999999999999999999);
-    alert(year);
+    var vector = Array(10);
+    //alert(year);
     console.log(decodeURIComponent("https://tmaq.mx"));
 } catch (error) {
     alert("A ocurrido un error");
