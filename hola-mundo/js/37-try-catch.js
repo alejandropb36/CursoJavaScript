9f6fcd96ac9059ccd3b23fad4f4c8798ba8e24cd
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-07-03 17:15:54 -0500

    Peticiones Asincronas: Captura de errores Try Catch

---
+++ hola-mundo/js/37-try-catch.js
@@ -0,0 +1,13 @@
+'use strict'
+
+try {
+    var year = 2019;
+
+    var vector = Array(99999999999999999999999999999);
+    alert(year);
+    console.log(decodeURIComponent("https://tmaq.mx"));
+} catch (error) {
+    alert("A ocurrido un error");
+    console.log(error);
+}
+
