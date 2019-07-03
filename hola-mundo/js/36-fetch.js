cd91d73516cb83c5557137129200512283c40f9e
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-07-03 17:15:15 -0500

    Peticiones Asincronas: Promesas

--- hola-mundo/js/36-fetch.js
+++ hola-mundo/js/36-fetch.js
@@ -2,7 +2,9 @@
 
 // Fetch (ajax) y peticiones a servicios / apis rest
 var div_usuarios = document.querySelector("#usuarios");
+var div_profesor = document.querySelector("#profesor");
 var div_usuario = document.querySelector("#usuario");
+
 
 /**
  * La promesa nos sirve para hacer multiples peticiones
@@ -13,11 +15,20 @@ getUsuarios()
     .then(users => {
         listadoUsuarios(users.data);
         // Se utiliza el return por la promesa
+        
+        return getInfo();
+    })
+    .then(data => {
+        div_profesor.innerHTML = data;
+        console.log(data);
         return getUsuario(2);
     })
     .then(data => data.json())
     .then(user => {
-        mostrarUsuario(user.data);
+        mostrarUsuario(user.data);   
+    })
+    .catch(error => {
+        console.log(error + " Este es el error!!!!!");
     });
 
 function getUsuarios(){
@@ -28,6 +39,24 @@ function getUsuario(id){
     var url = 'https://reqres.in/api/users/' + id;
     console.log(url);
     return fetch(url);
+}
+
+function getInfo(){
+    
+    var profesor = {
+        nombre: "Alejandro",
+        apellidos: "Ponce",
+        url: "alejandroponce.com"
+    };
+    return new Promise((resolve, reject) => {
+        var profesor_string = "";
+        setTimeout(function() {
+            profesor_string = JSON.stringify(profesor);
+            if(typeof profesor_string != 'string' || profesor_string == '') return reject('error 9871');
+            return resolve(profesor_string);
+        }, 3000);
+        
+    });
 }
 
 function listadoUsuarios(usuarios){
