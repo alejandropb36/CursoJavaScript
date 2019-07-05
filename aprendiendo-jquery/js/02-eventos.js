7c67d6addeed0e0f4bc45b52bfaf45302372ed7c
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-07-05 17:23:34 -0500

    Eventos en jQuery: Mouse move y mas ejemplos

--- aprendiendo-jquery/js/02-eventos.js
+++ aprendiendo-jquery/js/02-eventos.js
@@ -52,13 +52,34 @@ $(document).ready(function (){
      * Focus y blur
      */
     var nombre = $("#nombre");
-
+    var datos =  $("#datos");
     nombre.focus(function() {
         $(this).css("border", "3px solid green");
     });
     
     nombre.blur(function() {
         $(this).css("border", "1px solid ");
-        $("#datos").text($(this).val()).show();
+       datos.text($(this).val()).show();
     })
+
+    /**
+     * Mousedown y mouseup
+     */
+    datos.mousedown(function() {
+        $(this).css("border-color", "gray");
+    });
+
+    datos.mouseup(function() {
+        $(this).css("border-color", "black");
+    });
+
+    /**
+     * Mousemove
+     */
+    $(document).mousemove(function(){
+        $('body').css("cursor", "none");
+        var sigueme = $("#sigueme");
+        sigueme.css("left", event.clientX);
+        sigueme.css("top", event.clientY);
+    });
 });
