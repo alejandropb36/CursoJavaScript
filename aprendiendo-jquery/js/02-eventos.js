6649f22fe9c1cb1d7f638f93109344c710bc77d6
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-07-05 16:39:17 -0500

    Eventos en jQuery: Bluy y focus

--- aprendiendo-jquery/js/02-eventos.js
+++ aprendiendo-jquery/js/02-eventos.js
@@ -48,4 +48,17 @@ $(document).ready(function (){
                 .css("color", "yellow");
     });
 
+    /**
+     * Focus y blur
+     */
+    var nombre = $("#nombre");
+
+    nombre.focus(function() {
+        $(this).css("border", "3px solid green");
+    });
+    
+    nombre.blur(function() {
+        $(this).css("border", "1px solid ");
+        $("#datos").text($(this).val()).show();
+    })
 });
