f70db644d9c27e48ae68152fdc20de83d59bb0ea
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-07-05 12:20:39 -0500

    Eventos en jQuery: Hover

--- aprendiendo-jquery/js/02-eventos.js
+++ aprendiendo-jquery/js/02-eventos.js
@@ -4,14 +4,14 @@ $(document).ready(function (){
     console.log('jQuery y página listos !!');
 
     /**
-     *  ### MouseOver y MouseOut ###
+     * MouseOver y MouseOut.
      * Este tipo de animaciones se pueden hacer con CSS
      * pero cuando se require mas funcionalidades que solo
      * la animacion es mejor usa jQuery o JS, si no se 
      * requiere entonces CSS.
      */
     var caja = $("#caja");
-
+    /*
     caja.mouseover(function () {
         $(this).css('background', 'red');
     });
@@ -19,4 +19,19 @@ $(document).ready(function (){
     caja.mouseout(function() {
         $(this).css("background", "green");
     });
+    */
+
+    /**
+     * Hover.
+     * 
+     */
+    function cambiaRojo() {
+        $(this).css('background', 'red');
+    }
+
+    function cambiaVerde() {
+        $(this).css("background", "green");
+    }
+
+    caja.hover(cambiaRojo, cambiaVerde);
 });
