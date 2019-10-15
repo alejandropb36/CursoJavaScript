b3750cc92980d877440e0fd4ff79cc45f5fcc870
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-10-14 22:08:44 -0500

    Efectos, animaciones y mas: Animaciones perzonalizadas

--- aprendiendo-jquery/js/04-efectos.js
+++ aprendiendo-jquery/js/04-efectos.js
@@ -15,6 +15,32 @@ $(document).ready(function () {
 
     // slideToggle, fadeToggle y toggle
     $("#todoenuno").click(function () {
-        $("#caja").fadeToggle('normal');
+        caja.fadeToggle('normal');
+    });
+
+    $("#animar").click(function () {
+
+        caja.animate({
+                        marginLeft: '500px',
+                        fontSize: '40px',
+                        height: '110px'
+                    }, 'slow')
+            .animate({
+                        borderRadius: '900px',
+                        marginTop: '80px'
+                    }, 'slow')
+            .animate({
+                borderRadius: '0px',
+                marginLeft: '0px'
+            }, 'slow')
+            .animate({
+                borderRadius: '100px',
+                marginTop: '0px'
+            }, 'slow')
+            .animate({
+                marginLeft: '500px',
+                fontSize: '40px',
+                height: '110px'
+            }, 'slow');
     });
 });
