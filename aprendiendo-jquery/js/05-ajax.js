f6f4130bc6e1c990918535b4dcedee30859f8607
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-10-15 20:09:10 -0500

    Peticiones Ajax: Get

--- aprendiendo-jquery/js/05-ajax.js
+++ aprendiendo-jquery/js/05-ajax.js
@@ -1,4 +1,13 @@
 $(document).ready(function () {
     // Load
-    $("#datos").load("https://reqres.in/");
+    // $("#datos").load("https://reqres.in/");
+
+    // Get y Post
+    // $.get("https://reqres.in/api/users?page=2");
+    $.get("https://reqres.in/api/users", {page: 2}, function (response) {
+        console.log(response);
+        response.data.forEach((element, index) => {
+            $("#datos").append(("<p>" + element.first_name + " " + element.last_name + "</p>"));
+        });
+    });
 });
