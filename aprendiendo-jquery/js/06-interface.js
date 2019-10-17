626e0e065a0f0f5edd1d362ec571ee81b659e9f0
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-10-16 22:54:16 -0500

    Interacciones jQuery UI: Droppable

--- aprendiendo-jquery/js/06-interface.js
+++ aprendiendo-jquery/js/06-interface.js
@@ -15,4 +15,13 @@ $(document).ready(function () {
             console.log("Ha cambiado la lista !!!");
         }
     });
+
+    //  Drop
+    $("#elemento-movido").draggable();
+    $("#area").droppable({
+        drop: function () {
+            console.log("Haz soltado algo dentro del area");
+        }
+    });
+
 });
