a6d2dd168f0be33ca5abec8c86f5e806714e7337
Alejandro Ponce <alexponce36@hotmail.com>
2019-10-25 09:45:43 -0500

    Funcionalidad del proyecto: Acordeon

--- proyecto-js/js/main.js
+++ proyecto-js/js/main.js
@@ -1,68 +1,70 @@
 $(document).ready(function (){
-    $('.bxslider').bxSlider({
-        mode: 'fade',
-        captions: true,
-        slideWidth: 1200,
-        responsive: true
-    });
-
-    // Posts
-    var posts = [
-        {
-            title: "Prueba de titulo 1",
-            date: moment('2019-07-24 09:16:00').format("DD/MM/YYYY"),
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget arcu eros. Maecenas eget sem non ipsum auctor egestas. Nulla convallis ipsum vel neque consequat, eget aliquet felis congue. Integer sit amet augue vitae purus faucibus sodales at ut tortor. Etiam ut sapien mauris. Integer nibh nunc, accumsan nec malesuada a, viverra nec risus. Nullam a felis pellentesque, consectetur risus a, feugiat dolor." 
-        },
-        {
-            title: "Prueba de titulo 2",
-            date: moment().format("DD/MM/YYYY"),
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget arcu eros. Maecenas eget sem non ipsum auctor egestas. Nulla convallis ipsum vel neque consequat, eget aliquet felis congue. Integer sit amet augue vitae purus faucibus sodales at ut tortor. Etiam ut sapien mauris. Integer nibh nunc, accumsan nec malesuada a, viverra nec risus. Nullam a felis pellentesque, consectetur risus a, feugiat dolor." 
-        },
-        {
-            title: "Prueba de titulo 3",
-            date: moment().format("DD/MM/YYYY"),
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget arcu eros. Maecenas eget sem non ipsum auctor egestas. Nulla convallis ipsum vel neque consequat, eget aliquet felis congue. Integer sit amet augue vitae purus faucibus sodales at ut tortor. Etiam ut sapien mauris. Integer nibh nunc, accumsan nec malesuada a, viverra nec risus. Nullam a felis pellentesque, consectetur risus a, feugiat dolor." 
-        },
-        {
-            title: "Prueba de titulo 4",
-            date: moment().format("DD/MM/YYYY"),
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget arcu eros. Maecenas eget sem non ipsum auctor egestas. Nulla convallis ipsum vel neque consequat, eget aliquet felis congue. Integer sit amet augue vitae purus faucibus sodales at ut tortor. Etiam ut sapien mauris. Integer nibh nunc, accumsan nec malesuada a, viverra nec risus. Nullam a felis pellentesque, consectetur risus a, feugiat dolor." 
-        },
-        {
-            title: "Prueba de titulo 5",
-            date: moment().format("DD/MM/YYYY"),
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget arcu eros. Maecenas eget sem non ipsum auctor egestas. Nulla convallis ipsum vel neque consequat, eget aliquet felis congue. Integer sit amet augue vitae purus faucibus sodales at ut tortor. Etiam ut sapien mauris. Integer nibh nunc, accumsan nec malesuada a, viverra nec risus. Nullam a felis pellentesque, consectetur risus a, feugiat dolor." 
-        },
-        {
-            title: "Prueba de titulo 6",
-            date: moment().format("DD/MM/YYYY"),
-            content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget arcu eros. Maecenas eget sem non ipsum auctor egestas. Nulla convallis ipsum vel neque consequat, eget aliquet felis congue. Integer sit amet augue vitae purus faucibus sodales at ut tortor. Etiam ut sapien mauris. Integer nibh nunc, accumsan nec malesuada a, viverra nec risus. Nullam a felis pellentesque, consectetur risus a, feugiat dolor." 
-        }
-    ];
-
-    fetch("https://jsonplaceholder.typicode.com/posts?_limit=7")
-        .then((response) => {
-            console.log(response);
-            return response.json();
-        })
-        .then((posts) => {
-            console.log(posts);
-            posts.forEach((item,index) => {
-                var post = `
-                    <article class="post">
-                        <h2>${item.title}</h2>
-                        <span class="date">${moment().format("DD/MM/YYYY")}</span>
-                        <p>${item.body}</p>
-                        <a href="#" class="button-more">Leer más</a>
-                    </article>
-                `;
-                $("#posts").append(post);
-            });
-
-        })
-        .catch((error) => {
-            console.log(error.message);
+    if(window.location.href.indexOf('index') > -1){
+        $('.bxslider').bxSlider({
+            mode: 'fade',
+            captions: true,
+            slideWidth: 1200,
+            responsive: true
         });
+        // Posts
+        var posts = [
+            {
+                title: "Prueba de titulo 1",
+                date: moment('2019-07-24 09:16:00').format("DD/MM/YYYY"),
+                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget arcu eros. Maecenas eget sem non ipsum auctor egestas. Nulla convallis ipsum vel neque consequat, eget aliquet felis congue. Integer sit amet augue vitae purus faucibus sodales at ut tortor. Etiam ut sapien mauris. Integer nibh nunc, accumsan nec malesuada a, viverra nec risus. Nullam a felis pellentesque, consectetur risus a, feugiat dolor." 
+            },
+            {
+                title: "Prueba de titulo 2",
+                date: moment().format("DD/MM/YYYY"),
+                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget arcu eros. Maecenas eget sem non ipsum auctor egestas. Nulla convallis ipsum vel neque consequat, eget aliquet felis congue. Integer sit amet augue vitae purus faucibus sodales at ut tortor. Etiam ut sapien mauris. Integer nibh nunc, accumsan nec malesuada a, viverra nec risus. Nullam a felis pellentesque, consectetur risus a, feugiat dolor." 
+            },
+            {
+                title: "Prueba de titulo 3",
+                date: moment().format("DD/MM/YYYY"),
+                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget arcu eros. Maecenas eget sem non ipsum auctor egestas. Nulla convallis ipsum vel neque consequat, eget aliquet felis congue. Integer sit amet augue vitae purus faucibus sodales at ut tortor. Etiam ut sapien mauris. Integer nibh nunc, accumsan nec malesuada a, viverra nec risus. Nullam a felis pellentesque, consectetur risus a, feugiat dolor." 
+            },
+            {
+                title: "Prueba de titulo 4",
+                date: moment().format("DD/MM/YYYY"),
+                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget arcu eros. Maecenas eget sem non ipsum auctor egestas. Nulla convallis ipsum vel neque consequat, eget aliquet felis congue. Integer sit amet augue vitae purus faucibus sodales at ut tortor. Etiam ut sapien mauris. Integer nibh nunc, accumsan nec malesuada a, viverra nec risus. Nullam a felis pellentesque, consectetur risus a, feugiat dolor." 
+            },
+            {
+                title: "Prueba de titulo 5",
+                date: moment().format("DD/MM/YYYY"),
+                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget arcu eros. Maecenas eget sem non ipsum auctor egestas. Nulla convallis ipsum vel neque consequat, eget aliquet felis congue. Integer sit amet augue vitae purus faucibus sodales at ut tortor. Etiam ut sapien mauris. Integer nibh nunc, accumsan nec malesuada a, viverra nec risus. Nullam a felis pellentesque, consectetur risus a, feugiat dolor." 
+            },
+            {
+                title: "Prueba de titulo 6",
+                date: moment().format("DD/MM/YYYY"),
+                content: "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla eget arcu eros. Maecenas eget sem non ipsum auctor egestas. Nulla convallis ipsum vel neque consequat, eget aliquet felis congue. Integer sit amet augue vitae purus faucibus sodales at ut tortor. Etiam ut sapien mauris. Integer nibh nunc, accumsan nec malesuada a, viverra nec risus. Nullam a felis pellentesque, consectetur risus a, feugiat dolor." 
+            }
+        ];
+    
+        fetch("https://jsonplaceholder.typicode.com/posts?_limit=7")
+            .then((response) => {
+                console.log(response);
+                return response.json();
+            })
+            .then((posts) => {
+                console.log(posts);
+                posts.forEach((item,index) => {
+                    var post = `
+                        <article class="post">
+                            <h2>${item.title}</h2>
+                            <span class="date">${moment().format("DD/MM/YYYY")}</span>
+                            <p>${item.body}</p>
+                            <a href="#" class="button-more">Leer más</a>
+                        </article>
+                    `;
+                    $("#posts").append(post);
+                });
+    
+            })
+            .catch((error) => {
+                console.log(error.message);
+            });
+    }
+
     
     
 
@@ -108,4 +110,9 @@ $(document).ready(function (){
             location.reload();
         });
     }
+
+    if(window.location.href.indexOf('about') > -1){
+        $("#acordeon").accordion();
+    }
+
 });
