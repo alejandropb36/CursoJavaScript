3697cc53de131627626cc7c5e01d914284158739
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-10-26 12:42:38 -0500

    Variables y tipado TS: variables y tipado

---
+++ poo-y-typescript/js/03-tipos-de-datos.js
@@ -0,0 +1,22 @@
+// string
+var cadena = "Alejandro ponce";
+// cadena = 123;
+console.log(cadena);
+// Number
+var numero = 12;
+// numero = "cadena";
+console.log(numero);
+// Boleano
+var verdadero_falso = true;
+// verdadero_falso = 23;
+console.log(verdadero_falso);
+// Any
+var cualquier_cosa = "Hola como estas";
+cualquier_cosa = false;
+console.log(cualquier_cosa);
+// Arrays
+var lenguajes = ["PHP", "JS", "C#"];
+// lenguajes = [123,"Hola"];
+console.log(lenguajes);
+var years = [1, 2, 3, 4, 5];
+console.log(years);
---
+++ poo-y-typescript/js/03-tipos-de-datos.ts
@@ -0,0 +1,38 @@
+// string
+let cadena: string = "Alejandro ponce";
+
+// cadena = 123;
+
+console.log(cadena);
+
+// Number
+let numero: number = 12;
+
+// numero = "cadena";
+
+console.log(numero);
+
+// Boleano
+var verdadero_falso: boolean = true;
+
+// verdadero_falso = 23;
+
+console.log(verdadero_falso);
+
+// Any
+
+var cualquier_cosa: any = "Hola como estas";
+
+cualquier_cosa = false;
+
+console.log(cualquier_cosa);
+
+// Arrays
+var lenguajes : Array<string> = ["PHP", "JS", "C#"];
+
+// lenguajes = [123,"Hola"];
+
+console.log(lenguajes);
+
+let years: number[] = [1,2,3,4,5];
+console.log(years);
