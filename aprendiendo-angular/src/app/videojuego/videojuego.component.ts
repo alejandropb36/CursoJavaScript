066f0dfcc06db3ac66b5170a42d0d336f8b33ca4
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-11-11 21:10:47 -0600

    Componentes de angular: plantillas y propiedades

--- aprendiendo-angular/src/app/videojuego/videojuego.component.ts
+++ aprendiendo-angular/src/app/videojuego/videojuego.component.ts
@@ -2,19 +2,15 @@ import { Component} from '@angular/core';
 
 @Component({
     selector: 'videojuego',
-    template: `
-        <h2> Componente de videojuego </h2>
-        <ul> 
-            <li> GTA </li>
-            <li> principe de persia </li>
-            <li> Tekken </li>
-            <li> LOL </li>
-        </ul>
-    `
+    templateUrl: './videojuego.component.html'
 })
 
 export class VideojuegoComponent {
+    public titulo: string;
+    public listado: string;
     constructor() {
+        this.titulo = "Componente de Videojuegos";
+        this.listado = "Listado de videojuegos";
         console.log("Se cargo compoennte de videojuego");
     }
 }
