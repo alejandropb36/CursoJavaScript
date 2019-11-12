bfe7137eb882d78a2a87ebdc78fd503266e5800e
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-11-11 22:17:19 -0600

    Componentes de angular: hooks / eventos

--- aprendiendo-angular/src/app/app.component.ts
+++ aprendiendo-angular/src/app/app.component.ts
@@ -7,4 +7,9 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'aprendiendo-angular';
+  public mostrar_videojuegos: boolean = true;
+
+  ocultarVideojuegos(value){
+    this.mostrar_videojuegos = value;
+  }
 }
--- aprendiendo-angular/src/app/videojuego/videojuego.component.ts
+++ aprendiendo-angular/src/app/videojuego/videojuego.component.ts
@@ -1,16 +1,31 @@
-import { Component} from '@angular/core';
+import { Component, OnInit, DoCheck, OnDestroy } from '@angular/core';
 
 @Component({
     selector: 'videojuego',
     templateUrl: './videojuego.component.html'
 })
 
-export class VideojuegoComponent {
+export class VideojuegoComponent implements OnInit, DoCheck, OnDestroy {
     public titulo: string;
     public listado: string;
     constructor() {
         this.titulo = "Componente de Videojuegos";
         this.listado = "Listado de videojuegos";
         console.log("Se cargo compoennte de videojuego");
+    }
+
+    ngOnInit(){
+        console.log("OnInit de videojuegos!!");
+    }
+    ngDoCheck(){
+        console.log("DoCheck de videojuegos!!");
+    }
+
+    ngOnDestroy(){
+        console.log("OnDestroy ejecutado");
+    }
+
+    cambiarTitulo(){
+        this.titulo = "Nuevo Titulo";
     }
 }
