a26f0df2dc1a6fca7866f8fcc4ff5d9636f37b51
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-11-17 18:13:25 -0600

    Directivas estructurales: ngIf

--- aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
+++ aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -10,12 +10,19 @@ import { Zapatilla } from '../models/zapatilla';
 export class ZapatillasComponent implements OnInit {
     public titulo: string;
     public zapatillas: Array<Zapatilla>;
+    public deportivas: Array<Zapatilla>;
     constructor() {
         this.titulo = 'Componente de zapatillas en angular';
         this.zapatillas = [
             new Zapatilla("Charly gris", "Charly", "gris/azul", 699, true),
             new Zapatilla("Charly gis claro", "Charly", "gris claro", 699, true),
             new Zapatilla("Botas", "Alexa", "cafes", 599, true)
+        ];
+
+        this.deportivas = [
+            new Zapatilla("Nike r34", "Nike", "gris/azul", 699, true),
+            new Zapatilla("Adidas 334", "Adiddas", "gris claro", 699, true),
+            new Zapatilla("Pirma tuy6", "Pirma", "cafes", 599, true)
         ];
 
     }
