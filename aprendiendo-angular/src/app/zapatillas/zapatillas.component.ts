6d86dcb6c88c4a622263e38de1f8fb966b0bc07f
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-11-24 16:01:28 -0600

    Directivas estructurales: ngSwitch

--- aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
+++ aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -12,21 +12,24 @@ export class ZapatillasComponent implements OnInit {
     public zapatillas: Array<Zapatilla>;
     public deportivas: Array<Zapatilla>;
     public marcas: String[];
+    public color: string;
+
     constructor() {
+        this.color = 'blue';
         this.marcas = new Array();
         this.titulo = 'Componente de zapatillas en angular';
         this.zapatillas = [
-            new Zapatilla("Charly gris", "Charly", "gris/azul", 780, true),
-            new Zapatilla("Nike rtx345", "Nike", "gris/azul", 530, true),
-            new Zapatilla("running", "Adidas", "gris/azul", 1200, true),
-            new Zapatilla("Charly gis claro", "Charly", "gris claro", 399, false),
-            new Zapatilla("Botas", "Alexa", "cafes", 599, false)
+            new Zapatilla('Charly gris', 'Charly', 'gris/azul', 780, true),
+            new Zapatilla('Nike rtx345', 'Nike', 'gris/azul', 530, true),
+            new Zapatilla('running', 'Adidas', 'gris/azul', 1200, true),
+            new Zapatilla('Charly gis claro', 'Charly', 'gris claro', 399, false),
+            new Zapatilla('Botas', 'Alexa', 'cafes', 599, false)
         ];
 
         this.deportivas = [
-            new Zapatilla("Nike r34", "Nike", "gris/azul", 699, true),
-            new Zapatilla("Adidas 334", "Adiddas", "gris claro", 699, true),
-            new Zapatilla("Pirma tuy6", "Pirma", "cafes", 599, true)
+            new Zapatilla('Nike r34', 'Nike', 'gris/azul', 699, true),
+            new Zapatilla('Adidas 334', 'Adiddas', 'gris claro', 699, true),
+            new Zapatilla('Pirma tuy6', 'Pirma', 'cafes', 599, true)
         ];
 
     }
