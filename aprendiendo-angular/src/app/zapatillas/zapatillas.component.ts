f5ffb6623773a3f75a1bea5b6051140b51be8d4b
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-11-24 15:11:50 -0600

    Directivas estructurales: ngFor

--- aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
+++ aprendiendo-angular/src/app/zapatillas/zapatillas.component.ts
@@ -11,12 +11,16 @@ export class ZapatillasComponent implements OnInit {
     public titulo: string;
     public zapatillas: Array<Zapatilla>;
     public deportivas: Array<Zapatilla>;
+    public marcas: String[];
     constructor() {
+        this.marcas = new Array();
         this.titulo = 'Componente de zapatillas en angular';
         this.zapatillas = [
-            new Zapatilla("Charly gris", "Charly", "gris/azul", 699, true),
-            new Zapatilla("Charly gis claro", "Charly", "gris claro", 699, true),
-            new Zapatilla("Botas", "Alexa", "cafes", 599, true)
+            new Zapatilla("Charly gris", "Charly", "gris/azul", 780, true),
+            new Zapatilla("Nike rtx345", "Nike", "gris/azul", 530, true),
+            new Zapatilla("running", "Adidas", "gris/azul", 1200, true),
+            new Zapatilla("Charly gis claro", "Charly", "gris claro", 399, false),
+            new Zapatilla("Botas", "Alexa", "cafes", 599, false)
         ];
 
         this.deportivas = [
@@ -29,5 +33,17 @@ export class ZapatillasComponent implements OnInit {
 
     ngOnInit() {
         console.log(this.zapatillas);
+        this.getMarcas();
     }
+
+    getMarcas(){
+        this.zapatillas.forEach((zapatilla, index) => {
+            if(this.marcas.indexOf(zapatilla.marca) < 0){
+                this.marcas.push(zapatilla.marca);
+                console.log(index);
+            }
+        });
+        console.log( this.marcas);
+    }
+
 }
