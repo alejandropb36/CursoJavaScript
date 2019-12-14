ea258e47ef1eda0e7e3249c19dbad1a11c5a90cd
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-12-14 14:27:38 -0600

    Paginas y rutas: configurar el routing de angular

--- aprendiendo-angular/src/app/app.routing.ts
+++ aprendiendo-angular/src/app/app.routing.ts
@@ -1,6 +1,6 @@
-import { NgModule } from '@angular/core';
+// Importar modulos de router de angular
+import { ModuleWithProviders} from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
-
 
 // Importar componentes
 import { HomeComponent } from './home/home.component';
@@ -8,7 +8,8 @@ import { ZapatillasComponent } from './zapatillas/zapatillas.component';
 import { VideojuegoComponent } from './videojuego/videojuego.component';
 import { CursosComponent } from './cursos/cursos.component';
 
-const routes: Routes = [
+// Array de rutas
+const appRoutes: Routes = [
     {path: '', component: HomeComponent},
     {path: 'zapatillas', component: ZapatillasComponent },
     {path: 'videojuego', component: VideojuegoComponent},
@@ -16,8 +17,6 @@ const routes: Routes = [
     {path: '**', component: HomeComponent}
 ];
 
-@NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
-})
-export class AppRoutingModule { }
+// Exportar el modulo del router
+export const appRoutingProviders: any[] = [];
+export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
