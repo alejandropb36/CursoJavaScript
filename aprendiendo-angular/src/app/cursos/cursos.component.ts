071c4b1c9e741db665b5ba95a08e2840b919a66e
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-12-17 20:55:55 -0600

    Paginas y rutas en angular: Parametros por la url

--- aprendiendo-angular/src/app/cursos/cursos.component.ts
+++ aprendiendo-angular/src/app/cursos/cursos.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, ActivatedRoute, Params } from '@angular/router';
+
+
 
 @Component({
   selector: 'cursos',
@@ -7,9 +10,20 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  public nombre: string;
+  public followers: number;
+  constructor(
+    private _route: ActivatedRoute,
+    private _router: Router
+  ) { }
 
   ngOnInit() {
+    this._route.params.subscribe((params: Params) => {
+      // console.log(params);
+      this.nombre = params.nombre;
+      this.followers = +params.followers;
+      // console.log(typeof +params.followers);
+    });
   }
 
 }
