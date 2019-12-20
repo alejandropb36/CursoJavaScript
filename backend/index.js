dee70bfe0488147c5d23502c6b6a4d5597f8ddae
Alejandro Ponce <alexponce36@hotmail.com>
2019-12-20 13:57:03 -0600

    Backend con NodeJs: conexion a base de datos

--- backend/index.js
+++ backend/index.js
@@ -1,1 +1,13 @@
 'use strict'
+
+var mongoose = require('mongoose');
+
+mongoose.Promise = global.Promise;
+
+mongoose.connect('mongodb://localhost:27017/portafolio')
+    .then(() => {
+        console.log("Conexion a base de datos establecida con éxito!");
+    })
+    .catch(err => {
+        console.log(err);
+    });
