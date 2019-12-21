6cd9ee18fcfcdc9e329e84b62e849b4cfb8b1170
Luis Alejandro Ponce Brizuela <alexponce36@hotmail.com>
2019-12-20 18:46:05 -0600

    Backend con Nodejs: Servidor con nodejs

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,20 @@
 'use strict'
 
 var mongoose = require('mongoose');
+var app = require('./app');
+var port = 3700;
 
 mongoose.Promise = global.Promise;
 
 mongoose.connect('mongodb://localhost:27017/portafolio')
     .then(() => {
         console.log("Conexion a base de datos establecida con éxito!");
+
+        // Server
+        app.listen(port, () => {
+            console.log("Server running in url: http://localhost:3700");
+        });
+
     })
     .catch(err => {
         console.log(err);
