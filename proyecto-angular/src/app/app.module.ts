2b6734c5dc6305aecc224a0f0b0c85f6e8b65ca1
Alejandro Ponce <alexponce36@hotmail.com>
2019-12-23 11:00:54 -0600

    Pagina principal: Modelos y servicios

--- proyecto-angular/src/app/app.module.ts
+++ proyecto-angular/src/app/app.module.ts
@@ -1,6 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+// Http y forms
+import { HttpClientModule } from '@angular/common/http';
+import { FormsModule } from '@angular/forms';
+
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { AboutComponent } from './components/about/about.component';
